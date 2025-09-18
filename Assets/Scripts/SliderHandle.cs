d525b912c4fe3c93aebf0bae893a1f1621633b07
Codey Huntting <creativecodey@gmail.com>
2025-09-18 13:11:36 -0500

    had to change slider handle

--- Assets/Scripts/SliderHandle.cs
+++ Assets/Scripts/SliderHandle.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SliderHandle : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField]
+    private Transform _sizeTransform;
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        transform.localScale = new(4, 4, 4);
+        Grow();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        transform.localScale = Vector3.one;
+        Shrink();
+    }
+
+    private void Grow()
+    {
+        _sizeTransform.localScale = new(4, 4, 4);
+    }
+
+    private void Shrink()
+    {
+        _sizeTransform.localScale = Vector3.one;
     }
 }
